cd5a37de8949093bf9a55f7bcb21b1f06197bddd
Keren Park <parkhk10@gmail.com>
2020-03-04 14:49:11 -0600

    initial images on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+import SugarsThumb from './SugarsThumb';
+import AirbnbThumb from './AirbnbThumb';
+
 const Home = () => {
     return(
         <div className="">
@@ -12,11 +15,24 @@ const Home = () => {
                     <p>I’m a front-end developer and a design enthusiast.</p>
                     <p>Driven by passion for creating thoughtful experiences 
                         that empower others.</p>
+                    <div id="intro-icons">
+                        <FontAwesomeIcon icon={faEnvelope} className="fa-lg" id="envelope-icon"/>
+                        <FontAwesomeIcon icon={faGithub} className="fa-lg"id="github-icon"/>
+                        <FontAwesomeIcon icon={faLinkedin} className="fa-lg" id="linkedin-icon"/>
+                    </div>
                 </section> 
-                <div id="intro-icons">
-                    <FontAwesomeIcon icon={faEnvelope} className="fa-lg" id="envelope-icon"/>
-                    <FontAwesomeIcon icon={faGithub} className="fa-lg"id="github-icon"/>
-                    <FontAwesomeIcon icon={faLinkedin} className="fa-lg" id="linkedin-icon"/>
+                <br />
+                <br />
+                <br />
+                <div id="home-projects">
+                    <div className="project-display-row">
+                        <h4 className="project-display-title">Sugars Project</h4>
+                        <SugarsThumb />
+                    </div>
+                    <div className="project-display-row">
+                        <h4 className="project-display-title">Airbnb Project</h4>
+                        <AirbnbThumb />
+                    </div>
                 </div>
             </main>
         </div>
