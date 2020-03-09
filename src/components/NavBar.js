ced4bdac021d33393e03cf83fd85c39ba21feff2
Keren Park <parkhk10@gmail.com>
2020-03-09 11:46:39 -0500

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,16 +5,16 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
     return(
         <nav className="nav-bar">
-            <a href="/keren-portfolio"><Logo /></a>
+            <a href="/"><Logo /></a>
             <div className="">
                 <a id="about-menu" href="/about">
-                    <Link to="keren-portfolio/about">about</Link>
+                    <Link to="/about">about</Link>
                 </a> 
                 <a id="work-menu" href="/work">
-                    <Link to="keren-portfolio/work">work</Link>
+                    <Link to="/work">work</Link>
                 </a> 
                 <a id="play-menu" href="/play">
-                    <Link to="keren-portfolio/play">play</Link>
+                    <Link to="/play">play</Link>
                 </a> 
             </div>
         </nav>
