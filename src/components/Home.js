5f0c8da20a8ef517142f3f2c28dc88d68b665fe0
Keren Park <parkhk10@gmail.com>
2020-03-08 21:14:00 -0500

    add first draft of about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,15 +23,33 @@ const Home = () => {
                 </section> 
                 <br />
                 <br />
-                <br />
                 <div id="home-projects">
                     <div className="project-display-row">
-                        <h4 className="project-display-title">Sugars Project</h4>
                         <SugarsThumb />
+                        <div className="project-display-caption">
+                            <div>
+                                <h4 className="project-title">How do we digest sugars?</h4>
+                                <span>information visualization</span>
+                            </div>
+                        </div>
                     </div>
                     <div className="project-display-row">
-                        <h4 className="project-display-title">Airbnb Project</h4>
-                        <AirbnbThumb />
+                        <img src="./images/airbnb.png"/>
+                            <div className="project-display-caption">
+                                <div>
+                                    <h4 className="project-title">How can Airbnb increase customer loyalty?</h4>
+                                    <span>service design</span>
+                                </div>
+                            </div>
+                    </div>
+                    <div className="project-display-row">
+                        <img src="./images/majors.png"/>
+                            <div className="project-display-caption">
+                                <div>
+                                    <h4 className="project-title">How should I select my college major?</h4>
+                                    <span>information visualization</span>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </main>
